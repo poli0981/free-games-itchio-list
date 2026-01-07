f38873eabad86984c505e7e33d1f962f155b6cdf
poli0981 <127664709+poli0981@users.noreply.github.com>
2026-01-07 17:03:02 +0700

    add export_csv script for automated CSV updates

---
+++ bash/export_csv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install pandas
+python scripts/export_data.py
+ls -la
+git config --global user.name 'github-actions[bot]'
+git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'
+git status
+git add .
+git diff --staged --quiet && echo "No changes – skip commit" || (git commit -m "Auto update CSV [$(date +'%Y-%m-%d')] by GitHub Action" && git push)
